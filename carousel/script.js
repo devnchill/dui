7732ddb99e607ffa9b1204b634292f18fd936e2e
MONK <169875752+DevNChill@users.noreply.github.com>
2024-12-27 12:46:44 +0530

    feat: made left and right nav button work

--- carousel/script.js
+++ carousel/script.js
@@ -1,3 +1,21 @@
 function carousel() {
-  const imgContainer = document.getElementById("__img_container");
+  const leftNavBtn = document.getElementById("__left");
+  const rightNavBtn = document.getElementById("__right");
+  const imgRow = document.getElementById("__img_row");
+  let currentPosition = 0;
+  leftNavBtn.addEventListener("click", () => {
+    if (currentPosition === 0) {
+      currentPosition = -2500;
+    }
+    currentPosition += 500;
+    imgRow.style.left = `${currentPosition}px`;
+  });
+  rightNavBtn.addEventListener("click", () => {
+    if (currentPosition === -2000) {
+      currentPosition = 500;
+    }
+    currentPosition -= 500;
+    imgRow.style.left = `${currentPosition}px`;
+  });
 }
+carousel();
